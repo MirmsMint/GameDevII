6c74a4950a606c5a9dde6ee6188ed1475f4e16c0
MirmsMint <118831850+MirmsMint@users.noreply.github.com>
2024-02-19 17:38:34 -0500

    Raycast
    Made raycast appear when player points gun at object

---
+++ FirstPersonShooter/Assets/Guns/GunData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GunData", menuName = "Gun data")]
+public class GunData : ScriptableObject 
+{
+    public float range = 1000f;
+
+}
